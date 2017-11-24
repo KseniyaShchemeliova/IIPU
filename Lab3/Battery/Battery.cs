83fdcc7cfc3e817fe1d6c8dd07f269343f0ec8c3
Kseniya Shchemeliova <kseniya.shchamialiova@gmail.com>
2017-11-24 22:57:21 +0300

    add lab3

---
+++ Lab3/Battery/Battery.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Battery
+{
+    public partial class Battery : Form
+    {
+        private readonly BatteryManager _manager = new BatteryManager();
+
+        public Battery()
+        {
+            InitializeComponent();
+        }
+
+        //Method for initialization of states of app
+        private void BatteryLoad(object sender, EventArgs e)
+        {
+            //Getting the state of time of the battery
+            _manager.Init();
+            if (_manager.Charging == "Online") timeoutBox.Enabled = false;
+            UpdateBattery(null, null);
+            //Setting the event of timer
+            UpdateTimer.Tick += UpdateBattery;
+            UpdateTimer.Interval = 2000;
+            UpdateTimer.Start();
+            if (_manager.PreviousScreenTime > 300)
+                _manager.PreviousScreenTime = 300;
+            if (_manager.PreviousScreenTime == 0)
+                _manager.PreviousScreenTime = 1;
+            timeoutBox.SelectedIndex = timeoutBox.FindString(_manager.PreviousScreenTime.ToString());
+            timeoutSeconds.Text = (Int32.Parse(_manager.PreviousScreenTime.ToString()) * 60).ToString();
+            timeoutLabel.Text = "Время отключения дисплея " + timeoutBox.SelectedItem.ToString() + " минут.";
+        }
+
+        private void UpdateBattery(object sender, EventArgs e)
+        {
+            _manager.UpdateData();
+            State.Text = _manager.Charging;
+            Percentage.Text = _manager.PercentBattery;
+            timeLeft.Text = _manager.WorkTime;
+            //Checking if we need to unlock the dropdown
+            if (_manager.PreviousCharging != State.Text)
+            {
+                if (_manager.PreviousCharging == "Offline")
+                {
+                    timeoutBox.Enabled = false;
+                    timeoutSeconds.ReadOnly = true;
+                }
+                else
+                {
+                    timeoutBox.Enabled = true;
+                    timeoutSeconds.ReadOnly = false;
+                }
+                _manager.PreviousCharging = State.Text;
+            }
+        }
+
+        private void AppClosing(object sender, FormClosingEventArgs e)
+        {
+            _manager.SetDisplayOff(Int32.Parse(_manager.PreviousScreenTime.ToString()));
+        }
+
+        private void timeoutBox_ValueChanged(object sender, EventArgs e)
+        {
+            timeoutLabel.Text = "Время отключения дисплея " + timeoutBox.SelectedItem.ToString() + " минут.";
+            timeoutSeconds.Text = (Int32.Parse(timeoutBox.SelectedItem.ToString()) * 60).ToString();
+            _manager.SetDisplayOff(Int32.Parse(timeoutBox.SelectedItem.ToString()));
+        }
+
+        private void timeoutSeconds_ValueChanged(object sender, EventArgs e)
+        {
+            if (Int32.Parse(timeoutSeconds.Text) >= 60 && Int32.Parse(timeoutSeconds.Text) <= 18000)
+            {
+                timeoutLabel.Text = "Время отключения дисплея " + ((int)(Int32.Parse(timeoutSeconds.Text) / 60)).ToString() + " минут.";
+                _manager.SetDisplayOff((int)(Int32.Parse(timeoutSeconds.Text) / 60));
+            }
+            else
+            {
+                timeoutLabel.Text = "Время отключения дисплея 1 минут.";
+                timeoutSeconds.Text = "60";
+                _manager.SetDisplayOff(1);
+            }
+        }
+    }
+}
---
+++ Lab3/Battery/BatteryManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Battery
+{
+    public class BatteryManager
+    {
+        public int PreviousScreenTime { get; set; }
+        public string Charging { get; set; }
+        public string PercentBattery { get; set; }
+        public string WorkTime { get; set; }
+        public string PreviousCharging { get; set; }
+        public bool StartApp { get; set; }
+
+        public void Init()
+        {
+            // Запуск нашего приложения и получение состояния батареи
+            StartApp = true;
+            PreviousScreenTime = GetScreenTime();
+            // Обновление приложения
+            UpdateData();
+        }
+
+        // метод получения текущего состояния времени
+        public int GetScreenTime()
+        {
+            //Описание процесса для cmd
+            var procCmd = new Process();
+            procCmd.StartInfo.UseShellExecute = false;
+            procCmd.StartInfo.RedirectStandardOutput = true;
+            procCmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            procCmd.StartInfo.FileName = "cmd.exe";
+            procCmd.StartInfo.Arguments = "/c powercfg /q";
+            procCmd.Start();
+            //Из всего вывода нам нужен только VIDEOIDLE и последняя строка
+            var powerConfig = procCmd.StandardOutput.ReadToEnd();
+            var lastString = new Regex("VIDEOIDLE.*\\n.*\\n.*\\n.*\\n.*\\n.*\\n.*");
+            var videoIdle = lastString.Match(powerConfig).Value;
+            // После этого нам понадобится только 16 значений
+            var batteryState = videoIdle.Substring(videoIdle.Length - 11).TrimEnd();
+            //Преобразование string в Int
+            return Convert.ToInt32(batteryState, 16) / 60;
+        }
+        public void UpdateData()
+        {
+            // Получение текущего состояния
+            Charging = SystemInformation.PowerStatus.PowerLineStatus.ToString();
+            if (StartApp)
+            {
+                PreviousCharging = Charging;
+                StartApp = false;
+            }
+            // Получение текущего процента батареи
+            PercentBattery = SystemInformation.PowerStatus.BatteryLifePercent * 100 + "%";
+            // Если мы не используем AC
+            if (Charging == "Offline")
+            {
+                // Тогда мы можем начать вычислять время до разрядки
+                var calcLife = SystemInformation.PowerStatus.BatteryLifeRemaining;
+                // Если мы не получили значение, продолжаем вычислять
+                if (calcLife != -1)
+                {
+                    // Или инициализировать переменную для ее форматирования
+                    var span = new TimeSpan(0, 0, calcLife);
+                    WorkTime = span.ToString("g");
+                }                                                                      
+                else
+                {
+                    WorkTime = "Подсчет времени...";
+                }
+            }
+            else
+            {
+                WorkTime = "Устройство работает от сети.";
+            }
+        }
+
+        // Метод для изменения времени до отключения экрана
+        public void SetDisplayOff(int value)
+        {
+            // Описание процесса для cmd
+            var p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.StartInfo.Arguments = "/c powercfg /x -monitor-timeout-dc " + value;
+            p.Start();
+        }
+    }
+}
